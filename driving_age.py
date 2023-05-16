695abed51635dfa8503b6bf03c6bda0bb6585c64
Alan Chan <cf.alanpang@gmail.com>
2023-05-17 00:16:37 +0800

    second

---
+++ driving_age.py
@@ -0,0 +1,32 @@
+
+
+
+# this is for diving age
+
+
+driving = input('Did you drive before?  ')
+
+if driving != 'yes' and driving != 'no':
+
+	print ('please input yes or no')
+	raise SystemExit
+
+age = input('How old are you?  ')
+age = int(age)
+
+
+if driving == 'yes':
+	if age < 18:
+		print ('you are under 18')
+	else:
+		print ('cool man, enjoy your drive')
+elif driving == 'no':
+	if age < 18:
+		print ('good boy, you can get driving license soon.')
+	else:
+		print ('are you afraid of driving?')
+
+
+#python3 driving_age.py
+#cd /users/alanchan/documents/coding
+#cd /users/alanchan/desktop
